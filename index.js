1a2b49d0c703f14680a9f24e2f44c0f647c6ad00
Jatin Kumar <your-email@example.com>
2025-06-11 17:57:21 +0530

    url

---
+++ index.js
@@ -0,0 +1,48 @@
+
+import express from "express";
+import {nanoid} from "nanoid";
+import fs from "node:fs";
+
+const app = express();
+const Urls= {
+   B1sEbWh: 'https://www.moneycontrol.com/news/business/markets/leading-stock-exchanges-nse-and-bse-in-a-rack-race-to-ramp-up-co-location-capacities-as-demand-soars-13071282.html'
+}
+
+//Middleware
+app.use(express.urlencoded())
+
+
+app.get("/", (req, res)=>{
+    res.sendFile(import.meta.dirname + "/index.html"   )
+    
+})
+
+app.post("/shorten", (req, res)=>{
+        
+    // console.log(nanoid(7));
+    const shortUrl = nanoid(7);
+    Urls[shortUrl] = req.body.longUrl
+
+    const fileData = fs.readFileSync("urls.json")
+    const urlData = JSON.parse(fileData.toString()) 
+    urlData[shortUrl] = req.body.longUrl
+    fs.writeFileSync("urls.json", JSON.stringify(urlData, null, 2));
+    // console.log(urlData);
+    
+    
+    res.json({
+        success: true,
+        message: `http://localhost:7080/${shortUrl}`
+    })
+    
+})
+
+app.get("/:shortUrl", (req, res)=>{
+    const data = fs.readFileSync("urls.json")
+    const urlData = JSON.parse(data.toString())
+    const longUrl = urlData[req.params.shortUrl]
+    // console.log(longUrl)
+    res.redirect(longUrl);
+})
+
+app.listen(7080, () =>console.log("port number 7080 is run"));
