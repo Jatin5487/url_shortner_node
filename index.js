29a9437a3d8654564ec68271c68b6224fecf0df0
Jatin Kumar <your-email@example.com>
2025-06-11 22:27:54 +0530

    created gitignore

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 import express from "express";
 import {nanoid} from "nanoid";
 import fs from "node:fs";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const Urls= {
    B1sEbWh: 'https://www.moneycontrol.com/news/business/markets/leading-stock-exchanges-nse-and-bse-in-a-rack-race-to-ramp-up-co-location-capacities-as-demand-soars-13071282.html'
 }
@@ -11,6 +13,7 @@ const Urls= {
 //Middleware
 app.use(express.urlencoded())
 const PORT = process.env.PORT || 7080;
+
 
 app.get("/", (req, res)=>{
     res.sendFile(import.meta.dirname + "/index.html"   )
@@ -32,12 +35,14 @@ app.post("/shorten", (req, res)=>{
     
     res.json({
         success: true,
-        message: `http://localhost:7080/${shortUrl}`
+        message: `${process.env.BASE_PATH}/${shortUrl}`
     })
     
 })
 
 app.get("/:shortUrl", (req, res)=>{
+    
+    
     const data = fs.readFileSync("urls.json")
     const urlData = JSON.parse(data.toString())
     const longUrl = urlData[req.params.shortUrl]
